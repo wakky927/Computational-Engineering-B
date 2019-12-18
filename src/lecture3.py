0dd5824c799439bca502cbf5ec9696435904b5ac
Takuya <42827146+wakky927@users.noreply.github.com>
2019-12-18 15:52:40 +0900

    add lecture3 coding

--- src/lecture3.py
+++ src/lecture3.py
@@ -2,7 +2,6 @@ import os
 
 import numpy as np
 from matplotlib import pyplot as plt
-from scipy import optimize
 
 from func import diff, setup_conditions, fit
 
@@ -39,6 +38,19 @@ if __name__ == '__main__':
         print("{}, {}, {}, {}".format(t[i], f_exact[i],
                                       f_approx[i], error[i]))
 
+    # graph
+    fig, ax = plt.subplots()
+    ax.plot(t[:-1], f_exact[:-1], label='exact')
+    ax.plot(t[:-1], f_approx[:-1], label='approx_Euler')
+    ax.plot(t[:-1], error[:-1], label='error')
+    plt.title("Euler method")
+    plt.legend(loc='lower right')
+    plt.xlim(np.min(t), np.max(t))
+    plt.yscale("log")
+    # plt.show()
+    os.makedirs('../data/lecture3/images/Euler', exist_ok=True)
+    fig.savefig('../data/lecture3/images/Euler/dt_{}.png'.format(dt))
+
     ''' Adams-Bashforth method '''
     f_approx[0], f_approx[1] = f_exact[0], f_exact[1]  # I.C.
     diff.lec3_ab(n, dt, f_approx)
@@ -56,6 +68,19 @@ if __name__ == '__main__':
     for i in range(1, n):
         print("{}, {}, {}, {}".format(t[i], f_exact[i],
                                       f_approx[i], error[i]))
+
+    # graph
+    fig, ax = plt.subplots()
+    ax.plot(t[:-1], f_exact[:-1], label='exact')
+    ax.plot(t[:-1], f_approx[:-1], label='approx_Adams-Bashforth')
+    ax.plot(t[:-1], error[:-1], label='error')
+    plt.title("Adams-Bashforth method")
+    plt.legend(loc='lower right')
+    plt.xlim(np.min(t), np.max(t))
+    plt.yscale("log")
+    # plt.show()
+    os.makedirs('../data/lecture3/images/Adams-Bashforth', exist_ok=True)
+    fig.savefig('../data/lecture3/images/Adams-Bashforth/dt_{}.png'.format(dt))
 
     ''' Trapezoidal method '''
     f_approx[0], f_approx[1] = f_exact[0], f_exact[1]  # I.C.
@@ -75,3 +100,15 @@ if __name__ == '__main__':
         print("{}, {}, {}, {}".format(t[i], f_exact[i],
                                       f_approx[i], error[i]))
 
+    # graph
+    fig, ax = plt.subplots()
+    ax.plot(t[:-1], f_exact[:-1], label='exact')
+    ax.plot(t[:-1], f_approx[:-1], label='approx_Trapezoidal')
+    ax.plot(t[:-1], error[:-1], label='error')
+    plt.title("Trapezoidal method")
+    plt.legend(loc='lower right')
+    plt.xlim(np.min(t), np.max(t))
+    plt.yscale("log")
+    # plt.show()
+    os.makedirs('../data/lecture3/images/Trapezoidal', exist_ok=True)
+    fig.savefig('../data/lecture3/images/Trapezoidal/dt_{}.png'.format(dt))
